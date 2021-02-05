5c06b03fefe676509c9cd70e30f7216cd0f38b45
ParkChangJin-coder <brainnia@gmail.com>
2021-02-05 20:04:44 +0900

    2021-02-05

---
+++ Hello World.py
@@ -0,0 +1,7 @@
+print("Hello World!!")
+print("I dit it!")
+print("I made py file!! And it works!")
+print("Now I can make STUDY FILE!")
+
+# pyinstaller exe file create
+# ptinstaller -F Hello World.py
